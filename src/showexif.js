2f9b9feeec0489fb446f443fd59b738b645bf62c
ydx <x6doooo@gmail.com>
2013-07-16 16:18:01 +0800

    基本功能完成

--- src/showexif.js
+++ src/showexif.js
@@ -1,39 +1,59 @@
-$(function(){
+$(function() {
 
-  var showExifBtn = $('<div style="cursor:pointer;display:block;position:absolute;width:15px;height:15px;text-align:center;line-height:15px;background:#333;color:#fff;font-size:12px;z-index:99999">E</div>');
+	var showExifBtn = $('<div style="cursor:pointer;display:none;position:absolute;width:30px;height:30px;text-align:center;line-height:30px;background:#333;color:#fff;font-size:12px;z-index:9999">EXIF</div>');
 
-  var currentImg = null;
-  var timer = null;
+	showExifBtn.appendTo('body');
 
-  $('body').on('mouseover', 'img', function(){
-    currentImg = this;
-    var offset = $(this).offset();
-    showExifBtn.css({
-      top: offset.top + 'px',
-      left: offset.left - 15  + 'px'
-    });
-    showExifBtn.appendTo('body');
-  }); 
+	var currentImg = null;
+	var speed = 50;
+	var timer = false;
 
-  showExifBtn.click(function(){
-  
-    var exifWindow = window.open('about:blank',"_blank","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+	$('body').on('mouseover', 'img', function() {
+		clearTimeout(timer);
+		timer = null;
+		currentImg = this;
+		var offset = $(this).offset();
+		showExifBtn.css({
+			top: offset.top + 'px',
+			left: offset.left - 30 + 'px'
+		});
+		showExifBtn.show();
+	});
 
-    exifWindow.document.write('<p id="loading">加载中...</p>');
+	$('body').on('mouseout', 'img', function() {
+		timer = setTimeout(function() {
+			showExifBtn.hide();
+		},
+		speed);
+	});
 
-    EXIF.getData(currentImg, function(img){
-      var x = EXIF.getAllTags(currentImg);
+	showExifBtn.mouseover(function() {
+		clearTimeout(timer);
+		timer = null;
+	});
 
-      var str = '';
-      for(var key in x){
-        str += '<p style="line-height:160%; font-size:12px; "><b style="color:#666">' + key + '</b> : ' + x[key] + '</p>';
-      }
+	showExifBtn.mouseout(function() {
+		timer = setTimeout(function() {
+			showExifBtn.hide();
+		},
+		speed);
+	});
 
-      if(str == '') str = '无EXIF信息';
-      exifWindow.document.getElementById('loading').innerHTML = '<a href="' + currentImg.src + '">原图链接</a>';
-      exifWindow.document.write(str);
-    });
-  
-  });
+	showExifBtn.click(function() {
+		var exifWindow = window.open('about:blank', "_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=400, height=400");
+		exifWindow.document.write('<p id="loading">加载中...</p>');
+		EXIF.getData(currentImg, function(img) {
+			var x = EXIF.getAllTags(currentImg);
+			var str = '';
+			for (var key in x) {
+				str += '<p style="line-height:160%; font-size:12px; "><b style="color:#666">' + key + '</b> : ' + x[key] + '</p>';
+			}
+			if (str == '') str = '无EXIF信息';
+			exifWindow.document.getElementById('loading').innerHTML = '<a href="' + currentImg.src + '">原图链接</a>';
+			exifWindow.document.write(str);
+		});
+
+	});
 
 });
+
